0ef0446cb78831ead9664983c11e849f56a658eb
Ravi@036# <admin@admins-MacBook-Pro.local>
2023-05-29 21:56:26 +0530

    Added: valid first name

--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -1,11 +1,20 @@
 package com.bridgelabz;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UserRegistrationTest {
 
     @Test
     public void givenNameShouldReturnCorrectOrNot(){
-        System.out.println("Welcome to JUnit Testing.");
+        System.out.print("\nEnter First Name : ");
+        String string = "Sriram";
+        Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
+        Matcher matcher = pattern.matcher(string);
+        System.out.println(string);
+        Assertions.assertEquals(true,matcher.matches());
     }
 }
