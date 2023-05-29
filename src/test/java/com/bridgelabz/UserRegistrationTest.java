858f20748c3f1bbb75b3df90b981cfa7c1552f76
Ravi@036# <admin@admins-MacBook-Pro.local>
2023-05-29 21:40:21 +0530

    Added: JUnit Test Library

---
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Test;
+
+public class UserRegistrationTest {
+
+    @Test
+    public void givenNameShouldReturnCorrectOrNot(){
+        System.out.println("Welcome to JUnit Testing.");
+    }
+}
